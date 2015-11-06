a6974f802b6053b4c2d71ead1c1bcc7abc586808
Tu Dang <danghu@usi.ch>
2015-11-06 13:58:47 +0000

    multicast worked with multiple switches

--- scripts/receive.py
+++ scripts/receive.py
@@ -5,21 +5,29 @@ from paxos_header import *
 import struct
 import sys
 
+paxos_type = { 1: "prepare", 2: "promise", 3: "accept", 4: "accepted" }
+
 def handle_pkt(pkt):
-    print pkt.show2()
-    type_id = ord(pkt[Raw].load[0])
-    inst = "".join("{:02x}".format(ord(c)) for c in str(pkt[Raw].load[1:3]))
-    inst = int(inst, 16)
-    rnd = ord(pkt[Raw].load[3])
-    vrnd = ord(pkt[Raw].load[4])
-    value = str(pkt[Raw].load[4:9])
-    payload = pkt[Raw].load[9:]
-    paxos_type = { 1: "prepare", 2: "promise", 3: "accept", 4: "accepted" }
-    #print "| %10s | %4d |  %02x | %02x | %04s | %s |" % (paxos_type[type_id], inst, rnd, vrnd, value, payload)
+    try:
+        if pkt['IP'].proto != 0x11:
+            return
+        datagram = pkt['Raw'].load
+        fmt = '>' + 'b h b b 3s'
+        packer = struct.Struct(fmt)
+        packed_size = struct.calcsize(fmt)
+        unpacked_data = packer.unpack(datagram[:packed_size])
+        remaining_payload = datagram[packed_size:]
+        typ, inst, rnd, vrnd, value = unpacked_data
+        print "| %10s | %4d |  %02x | %02x | %04s | %s |" % \
+                (paxos_type[typ], inst, rnd, vrnd, value, remaining_payload)
+    except IndexError as ex:
+        print ex
+
 
 def main():
-    print "| %10s | %4s |  %2s | %2s | %4s | %s |" % ("type", "inst", "pr", "ar", "val", "payload")
-    sniff(filter="udp && dst port 34952", iface = sys.argv[1],
+    print "| %10s | %4s |  %2s | %2s | %4s | %s |" % \
+         ("type", "inst", "pr", "ar", "val", "payload")
+    sniff(filter="udp && dst port 34952",
           prn = lambda x: handle_pkt(x))
 
 if __name__ == '__main__':
