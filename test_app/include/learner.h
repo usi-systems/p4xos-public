1dcb20c4301d88b7e61a7599ff038762c8a7ee7c
Tu Dang <tudang87@gmail.com>
2016-01-14 14:20:12 +0100

    include faked performance measurement

--- test_app/include/learner.h
+++ test_app/include/learner.h
@@ -1,5 +1,10 @@
 #ifndef _LEARNER_H
 #define _LEARNER_H
 
+typedef struct Stat {
+    int mps;
+    float avg_lat;
+} Stat;
+
 int start_learner();
 #endif
--- test_app/src/learner.c
+++ test_app/src/learner.c
@@ -13,22 +13,36 @@
 #define LEARNER_PORT 34952
 #define BUFSIZE 1470
 
+
+void monitor(evutil_socket_t fd, short what, void *arg) {
+    Stat *stat = (Stat *) arg;
+    printf("mps: %d, avg_lat: %f\n", stat->mps, stat->avg_lat);
+
+}
+
+
 void cb_func(evutil_socket_t fd, short what, void *arg)
 {
+    Stat *stat = (Stat *) arg;
     Message msg;
     struct sockaddr_in remote;
     socklen_t remote_len = sizeof(remote);
     int n = recvfrom(fd, &msg, sizeof(msg), 0, (struct sockaddr *) &remote, &remote_len);
     if (n < 0)
       perror("ERROR in recvfrom");
-    printf("Received packet from %s:%d\n", inet_ntoa(remote.sin_addr), ntohs(remote.sin_port));
+    printf("Received %d bytes from %s:%d\n", n, inet_ntoa(remote.sin_addr), ntohs(remote.sin_port));
     char buf[BUFSIZE];
-    message_to_string(msg, buf);
+    Message m = decode_message(msg);
+    message_to_string(m, buf);
     printf("%s" , buf);
+    stat->mps++;
+    stat->avg_lat += 0.1;
 }
 
 int start_learner() {
     struct event_base *base = event_base_new();
+    Stat stat = {0, 0.0};
+
     int fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (fd < 0) {
         perror("cannot create socket");
@@ -41,9 +55,12 @@ int start_learner() {
     // bindSocketToDevice(fd, "eth2");
     if (bind(fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
         perror("ERROR on binding");
-    struct event *ev1;
-    ev1 = event_new(base, fd, EV_READ|EV_PERSIST, cb_func, NULL);
-    event_add(ev1, NULL);
+    struct event *recv_ev, *monitor_ev;
+    recv_ev = event_new(base, fd, EV_READ|EV_PERSIST, cb_func, &stat);
+    struct timeval timeout = {1, 0};
+    monitor_ev = event_new(base, 0, EV_TIMEOUT|EV_PERSIST, monitor, &stat);
+    event_add(recv_ev, NULL);
+    event_add(monitor_ev, &timeout);
     event_base_dispatch(base);
     return 0;
 }
