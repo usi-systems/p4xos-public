b216a6245e76e2119527da8cc6d23c8648069798
Tu Dang <huynh.tu.dang@usi.ch>
2016-02-04 14:07:12 +0100

    match on paxos message type to select the right paxos handler

--- netrodemo/paxos.c
+++ netrodemo/paxos.c
@@ -39,12 +39,7 @@ int pif_plugin_seq_func(EXTRACTED_HEADERS_T *headers, MATCH_DATA_T *data)
     return PIF_PLUGIN_RETURN_FORWARD;
 }
 
-/*
- * Can we get the remaining packet payload after all parsed headers?
- * For example, after parsing, we have ethernet, ipv4, paxos headers.
- * Can we extract the bytes after the paxos header ?
- */
-int pif_plugin_paxos_func(EXTRACTED_HEADERS_T *headers, MATCH_DATA_T *data)
+int pif_plugin_paxos_phase1a(EXTRACTED_HEADERS_T *headers, MATCH_DATA_T *data)
 {
     PIF_PLUGIN_paxos_T *paxos;
     PIF_PLUGIN_value_T *value;
@@ -53,51 +48,60 @@ int pif_plugin_paxos_func(EXTRACTED_HEADERS_T *headers, MATCH_DATA_T *data)
         return PIF_PLUGIN_RETURN_DROP;
     }
 
+    if (! pif_plugin_hdr_value_present(headers)) {
+        return PIF_PLUGIN_RETURN_DROP;
+    }
+
     paxos = pif_plugin_hdr_get_paxos(headers);
+    value = pif_plugin_hdr_get_value(headers);
 
     pkt_inst = paxos->inst;
 
-    switch (paxos->msgtype) {
-        case Phase1A:
-            // handle Phase1A
-            if (paxos->rnd >= rnds[pkt_inst]) {
-                rnds[pkt_inst]  = paxos->rnd;
-                paxos->vrnd = vrnds[pkt_inst];
-                paxos->valsize = valsizes[pkt_inst];
-                paxos->acpt = SWITCH_ID;
-                paxos->msgtype = Phase1B;
-                /*
-                 * How to attach a header (e.g, value header) to the packet ?
-                 * add_header(value);
-                 * Copy accepted value from switch memory to packet.
-                 * value->content = values[pkt_inst];
-                */
-            }
-            break;
-        case Phase1B:
-            // handle Phase1B
-            break;
-        case Phase2A:
-            // handle Phase2A
-            if (paxos->rnd >= rnds[pkt_inst]) {
-                rnds[pkt_inst]  = paxos->rnd;
-                vrnds[pkt_inst] = paxos->rnd;
-                valsizes[pkt_inst] = paxos->valsize;
-                paxos->acpt = SWITCH_ID;
-                paxos->msgtype = Phase2B;
-                if (! pif_plugin_hdr_value_present(headers)) {
-                    return PIF_PLUGIN_RETURN_DROP;
-                }
-                value = pif_plugin_hdr_get_value(headers);
-                values[pkt_inst] = value->content;
-            }
-            break;
-        case Phase2B:
-            // handle Phase2B
-            break;
-        default:
-            break;
-        }
+    if (paxos->rnd < rnds[pkt_inst]) {
+        return PIF_PLUGIN_RETURN_DROP;
+    }
+    rnds[pkt_inst]  = paxos->rnd;
+    paxos->vrnd = vrnds[pkt_inst];
+    paxos->valsize = valsizes[pkt_inst];
+    paxos->acpt = SWITCH_ID;
+    paxos->msgtype = Phase1B;
+    value->content = values[pkt_inst];
+
+    return PIF_PLUGIN_RETURN_FORWARD;
+}
+
+/*
+ * Can we get the remaining packet payload after all parsed headers?
+ * For example, after parsing, we have ethernet, ipv4, paxos headers.
+ * Can we extract the bytes after the paxos header ?
+ */
+int pif_plugin_paxos_phase2a(EXTRACTED_HEADERS_T *headers, MATCH_DATA_T *data)
+{
+    PIF_PLUGIN_paxos_T *paxos;
+    PIF_PLUGIN_value_T *value;
+
+    if (! pif_plugin_hdr_paxos_present(headers)) {
+        return PIF_PLUGIN_RETURN_DROP;
+    }
+
+    if (! pif_plugin_hdr_value_present(headers)) {
+        return PIF_PLUGIN_RETURN_DROP;
+    }
+
+    paxos = pif_plugin_hdr_get_paxos(headers);
+    value = pif_plugin_hdr_get_value(headers);
+
+    pkt_inst = paxos->inst;
+
+    if (paxos->rnd < rnds[pkt_inst]) {
+        return PIF_PLUGIN_RETURN_DROP;
+    }
+    rnds[pkt_inst]  = paxos->rnd;
+    vrnds[pkt_inst] = paxos->rnd;
+    valsizes[pkt_inst] = paxos->valsize;
+    paxos->acpt = SWITCH_ID;
+    paxos->msgtype = Phase2B;
+    values[pkt_inst] = value->content;
 
     return PIF_PLUGIN_RETURN_FORWARD;
 }
