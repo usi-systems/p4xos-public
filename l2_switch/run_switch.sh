2bd2b8f68f1f5499e70cbbc4423edf9fe7255769
Tu Dang <huynh.tu.dang@usi.ch>
2016-02-18 15:16:59 +0100

    change path env.sh

--- l2_switch/run_switch.sh
+++ l2_switch/run_switch.sh
@@ -2,7 +2,7 @@
 
 PROG="l2switch"
 
-source env.sh
+source ../debug/env.sh
 $P4C_BM_SCRIPT ../p4src/$PROG.p4 --json $PROG.json
 sudo PYTHONPATH=$PYTHONPATH:$BMV2_PATH/mininet/ python topo.py \
     --behavioral-exe $BMV2_PATH/targets/simple_switch/simple_switch \
