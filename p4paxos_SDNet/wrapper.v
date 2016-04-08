01c43db7d2fa870cd32b620ece6a3ec30ad726df
pbghb <pietro.bressana@gmail.com>
2016-04-08 10:17:52 +0200

    minor changes in wrapper code

--- p4paxos_SDNet/wrapper.v
+++ p4paxos_SDNet/wrapper.v
@@ -82,141 +82,159 @@ output													S_AXI_AWREADY
 //#################################
 
 // AXIS PACKET INPUT INTERFACE
-wire		[0:0]										reg_packet_in_packet_in_TVALID ;
-wire 		[0:0]										reg_packet_in_packet_in_TREADY ;
-wire		[255:0]										reg_packet_in_packet_in_TDATA ;
-wire		[31:0]										reg_packet_in_packet_in_TKEEP ;
-wire		[0:0]										reg_packet_in_packet_in_TLAST ;
+wire		[0:0]										wire_packet_in_packet_in_TVALID ;
+wire 		[0:0]										wire_packet_in_packet_in_TREADY ;
+wire		[255:0]										wire_packet_in_packet_in_TDATA ;
+wire		[31:0]										wire_packet_in_packet_in_TKEEP ;
+wire		[0:0]										wire_packet_in_packet_in_TLAST ;
 
 // AXI-LITE CONTROL INTERFACE
-wire		[8:0] 										reg_control_S_AXI_AWADDR ;
-wire		[0:0] 										reg_control_S_AXI_AWVALID ;
-wire		[0:0] 										reg_control_S_AXI_AWREADY ;
-wire		[31:0] 										reg_control_S_AXI_WDATA ;
-wire 		[3:0] 										reg_control_S_AXI_WSTRB ;
-wire 		[0:0] 										reg_control_S_AXI_WVALID ;
-wire 		[0:0] 										reg_control_S_AXI_WREADY ;
-wire 		[1:0] 										reg_control_S_AXI_BRESP ;
-wire 		[0:0] 										reg_control_S_AXI_BVALID ;
-wire 		[0:0] 										reg_control_S_AXI_BREADY ;
-wire 		[8:0] 										reg_control_S_AXI_ARADDR ;
-wire 		[0:0] 										reg_control_S_AXI_ARVALID ;
-wire 		[0:0] 										reg_control_S_AXI_ARREADY ;
-wire 		[31:0]										reg_control_S_AXI_RDATA ;
-wire 		[1:0] 										reg_control_S_AXI_RRESP ;
-wire 		[0:0] 										reg_control_S_AXI_RVALID ;
-wire 		[0:0] 										reg_control_S_AXI_RREADY ;
+wire		[8:0] 										wire_control_S_AXI_AWADDR ;
+wire		[0:0] 										wire_control_S_AXI_AWVALID ;
+wire		[0:0] 										wire_control_S_AXI_AWREADY ;
+wire		[31:0] 										wire_control_S_AXI_WDATA ;
+wire 		[3:0] 										wire_control_S_AXI_WSTRB ;
+wire 		[0:0] 										wire_control_S_AXI_WVALID ;
+wire 		[0:0] 										wire_control_S_AXI_WREADY ;
+wire 		[1:0] 										wire_control_S_AXI_BRESP ;
+wire 		[0:0] 										wire_control_S_AXI_BVALID ;
+wire 		[0:0] 										wire_control_S_AXI_BREADY ;
+wire 		[8:0] 										wire_control_S_AXI_ARADDR ;
+wire 		[0:0] 										wire_control_S_AXI_ARVALID ;
+wire 		[0:0] 										wire_control_S_AXI_ARREADY ;
+wire 		[31:0]										wire_control_S_AXI_RDATA ;
+wire 		[1:0] 										wire_control_S_AXI_RRESP ;
+wire 		[0:0] 										wire_control_S_AXI_RVALID ;
+wire 		[0:0] 										wire_control_S_AXI_RREADY ;
 
 // AXIS PACKET OUTPUT INTERFACE
-wire 		[0:0] 										reg_packet_out_packet_out_TVALID ;
-wire 		[0:0] 										reg_packet_out_packet_out_TREADY ;
-wire 		[255:0]										reg_packet_out_packet_out_TDATA ;
-wire 		[31:0]										reg_packet_out_packet_out_TKEEP ;
-wire 		[0:0] 										reg_packet_out_packet_out_TLAST ;
+wire 		[0:0] 										wire_packet_out_packet_out_TVALID ;
+wire 		[0:0] 										wire_packet_out_packet_out_TREADY ;
+wire 		[255:0]										wire_packet_out_packet_out_TDATA ;
+wire 		[31:0]										wire_packet_out_packet_out_TKEEP ;
+wire 		[0:0] 										wire_packet_out_packet_out_TLAST ;
 
 // LINE CLK & RST SIGNALS
 reg 													reg_clk_line_rst ; // INV
-wire 													reg_clk_line ;
+wire 													wire_clk_line ;
 
 // PACKET CLK & RST SIGNALS
 reg 													reg_clk_packet_rst ; // INV
-wire 													reg_clk_packet ;
+wire 													wire_clk_packet ;
 
 // CONTROL CLK & RST SIGNALS
 reg 													reg_clk_control_rst ; // INV
-wire 													reg_clk_control ;
+wire 													wire_clk_control ;
 
 //#################################
 //####       CONNECTIONS
 //#################################
 
 // AXIS PACKET INPUT INTERFACE
-assign 		reg_packet_in_packet_in_TVALID = s_axis_tvalid ;
-assign 		reg_packet_in_packet_in_TREADY = s_axis_tready;
-assign 		reg_packet_in_packet_in_TDATA = s_axis_tdata;
-assign 		reg_packet_in_packet_in_TKEEP = s_axis_tkeep;
-assign 		reg_packet_in_packet_in_TLAST = s_axis_tlast;
+assign 		wire_packet_in_packet_in_TVALID = s_axis_tvalid ;
+assign 		wire_packet_in_packet_in_TREADY = s_axis_tready;
+assign 		wire_packet_in_packet_in_TDATA = s_axis_tdata;
+assign 		wire_packet_in_packet_in_TKEEP = s_axis_tkeep;
+assign 		wire_packet_in_packet_in_TLAST = s_axis_tlast;
 
 // AXI-LITE CONTROL INTERFACE
-assign		reg_control_S_AXI_AWADDR = S_AXI_AWADDR; // [REG]<--INPUT
-assign		reg_control_S_AXI_AWVALID = S_AXI_AWVALID ; // [REG]<--INPUT
-assign		S_AXI_AWREADY = reg_control_S_AXI_AWREADY; // OUTPUT<--[REG]
-assign		reg_control_S_AXI_WDATA = S_AXI_WDATA ; // [REG]<--INPUT
-assign		reg_control_S_AXI_WSTRB = S_AXI_WSTRB ; // [REG]<--INPUT
-assign		reg_control_S_AXI_WVALID = S_AXI_WVALID ; // [REG]<--INPUT
-assign		S_AXI_WREADY = reg_control_S_AXI_WREADY ; // OUTPUT<--[REG]
-assign		S_AXI_BRESP = reg_control_S_AXI_BRESP ; // OUTPUT<--[REG]
-assign		S_AXI_BVALID = reg_control_S_AXI_BVALID ; // OUTPUT<--[REG]
-assign		reg_control_S_AXI_BREADY = S_AXI_BREADY; // [REG]<--INPUT
-assign		reg_control_S_AXI_ARADDR = S_AXI_ARADDR; // [REG]<--INPUT
-assign		reg_control_S_AXI_ARVALID = S_AXI_ARVALID ; // [REG]<--INPUT
-assign		S_AXI_ARREADY = reg_control_S_AXI_ARREADY ; // OUTPUT<--[REG]
-assign		S_AXI_RDATA = reg_control_S_AXI_RDATA ; // OUTPUT<--[REG]
-assign		S_AXI_RRESP = reg_control_S_AXI_RRESP ; // OUTPUT<--[REG]
-assign		S_AXI_RVALID = reg_control_S_AXI_RVALID ; // OUTPUT<--[REG]
-assign		reg_control_S_AXI_RREADY = S_AXI_RREADY; // [REG]<--INPUT
+assign		wire_control_S_AXI_AWADDR = S_AXI_AWADDR; // [REG]<--INPUT
+assign		wire_control_S_AXI_AWVALID = S_AXI_AWVALID ; // [REG]<--INPUT
+assign		S_AXI_AWREADY = wire_control_S_AXI_AWREADY; // OUTPUT<--[REG]
+assign		wire_control_S_AXI_WDATA = S_AXI_WDATA ; // [REG]<--INPUT
+assign		wire_control_S_AXI_WSTRB = S_AXI_WSTRB ; // [REG]<--INPUT
+assign		wire_control_S_AXI_WVALID = S_AXI_WVALID ; // [REG]<--INPUT
+assign		S_AXI_WREADY = wire_control_S_AXI_WREADY ; // OUTPUT<--[REG]
+assign		S_AXI_BRESP = wire_control_S_AXI_BRESP ; // OUTPUT<--[REG]
+assign		S_AXI_BVALID = wire_control_S_AXI_BVALID ; // OUTPUT<--[REG]
+assign		wire_control_S_AXI_BREADY = S_AXI_BREADY; // [REG]<--INPUT
+assign		wire_control_S_AXI_ARADDR = S_AXI_ARADDR; // [REG]<--INPUT
+assign		wire_control_S_AXI_ARVALID = S_AXI_ARVALID ; // [REG]<--INPUT
+assign		S_AXI_ARREADY = wire_control_S_AXI_ARREADY ; // OUTPUT<--[REG]
+assign		S_AXI_RDATA = wire_control_S_AXI_RDATA ; // OUTPUT<--[REG]
+assign		S_AXI_RRESP = wire_control_S_AXI_RRESP ; // OUTPUT<--[REG]
+assign		S_AXI_RVALID = wire_control_S_AXI_RVALID ; // OUTPUT<--[REG]
+assign		wire_control_S_AXI_RREADY = S_AXI_RREADY; // [REG]<--INPUT
 
 // AXIS PACKET OUTPUT INTERFACE
-assign  	m_axis_tvalid	=   reg_packet_out_packet_out_TVALID ;
-assign  	m_axis_tready	=	reg_packet_out_packet_out_TREADY ;
-assign  	m_axis_tdata	=	reg_packet_out_packet_out_TDATA ;
-assign  	m_axis_tkeep	=	reg_packet_out_packet_out_TKEEP ;
-assign  	m_axis_tlast	=	reg_packet_out_packet_out_TLAST ;
+assign  	m_axis_tvalid	=   wire_packet_out_packet_out_TVALID ;
+assign  	m_axis_tready	=	wire_packet_out_packet_out_TREADY ;
+assign  	m_axis_tdata	=	wire_packet_out_packet_out_TDATA ;
+assign  	m_axis_tkeep	=	wire_packet_out_packet_out_TKEEP ;
+assign  	m_axis_tlast	=	wire_packet_out_packet_out_TLAST ;
 
 // LINE CLK & RST SIGNALS
-assign 		reg_clk_line = axis_aclk ;
+assign 		wire_clk_line = axis_aclk ;
 
 // PACKET CLK & RST SIGNALS
-assign 		reg_clk_packet = axis_aclk ;
+assign 		wire_clk_packet = axis_aclk ;
 
 // CONTROL CLK & RST SIGNALS
-assign  	reg_clk_control = S_AXI_ACLK ;
+assign  	wire_clk_control = S_AXI_ACLK ;
 
 //###################################
-//####       P4_PROCESSOR INSTANCE
+//####    AXI_CONTROL INSTANCE
+//###################################
+
+// TODO:
+// HW MODULE THAT MAPS SUME AXI CONTROL
+// ADDRESS TO P4_PROCESSOR AXI CONTROL ADDRESS
+// INSTEAD OF DISCARDING EXCEEDING bits
+
+//###################################
+//####    TUSER_FSM INSTANCE
+//###################################
+
+// TODO:
+// HW MODULE THAT MAPS SUME TUSER SIGNAL
+// TO P4_PROCESSOR TUPLE PORT
+// INSTEAD OF SENDING IT TO THE OUTOUT PORT
+
+//###################################
+//####    P4_PROCESSOR INSTANCE
 //###################################
 
 p4_processor p4_processor_inst (
 
 // AXIS PACKET INPUT INTERFACE
-.packet_in_packet_in_TVALID								(reg_packet_in_packet_in_TVALID),
-.packet_in_packet_in_TREADY								(reg_packet_in_packet_in_TREADY),
-.packet_in_packet_in_TDATA								(reg_packet_in_packet_in_TDATA),
-.packet_in_packet_in_TKEEP								(reg_packet_in_packet_in_TKEEP),
-.packet_in_packet_in_TLAST								(reg_packet_in_packet_in_TLAST),
+.packet_in_packet_in_TVALID								(wire_packet_in_packet_in_TVALID),
+.packet_in_packet_in_TREADY								(wire_packet_in_packet_in_TREADY),
+.packet_in_packet_in_TDATA								(wire_packet_in_packet_in_TDATA),
+.packet_in_packet_in_TKEEP								(wire_packet_in_packet_in_TKEEP),
+.packet_in_packet_in_TLAST								(wire_packet_in_packet_in_TLAST),
 
 // TUPLE INPUT INTERFACE
 .tuple_in_tuple_in_VALID								(1'b0), // CONSTANT
 .tuple_in_tuple_in_DATA									(128'b0), // CONSTANT
 
 // AXI-LITE CONTROL INTERFACE
-.control_S_AXI_AWADDR									(reg_control_S_AXI_AWADDR [8:0]  ), // MISMATCH [11 : 9]
-.control_S_AXI_AWVALID									(reg_control_S_AXI_AWVALID ),
-.control_S_AXI_AWREADY									(reg_control_S_AXI_AWREADY ),
-.control_S_AXI_WDATA									(reg_control_S_AXI_WDATA ),
-.control_S_AXI_WSTRB									(reg_control_S_AXI_WSTRB  ),
-.control_S_AXI_WVALID									(reg_control_S_AXI_WVALID  ),
-.control_S_AXI_WREADY									(reg_control_S_AXI_WREADY  ),
-.control_S_AXI_BRESP									(reg_control_S_AXI_BRESP  ),
-.control_S_AXI_BVALID									(reg_control_S_AXI_BVALID  ),
-.control_S_AXI_BREADY									(reg_control_S_AXI_BREADY  ),
-.control_S_AXI_ARADDR									(reg_control_S_AXI_ARADDR [8:0] ), // MISMATCH [11 : 9]
-.control_S_AXI_ARVALID									(reg_control_S_AXI_ARVALID ),
-.control_S_AXI_ARREADY									(reg_control_S_AXI_ARREADY ),
-.control_S_AXI_RDATA									(reg_control_S_AXI_RDATA  ),
-.control_S_AXI_RRESP									(reg_control_S_AXI_RRESP  ),
-.control_S_AXI_RVALID									(reg_control_S_AXI_RVALID  ),
-.control_S_AXI_RREADY									(reg_control_S_AXI_RREADY  ),
+.control_S_AXI_AWADDR									(wire_control_S_AXI_AWADDR [8:0]  ), // MISMATCH [11 : 9]
+.control_S_AXI_AWVALID									(wire_control_S_AXI_AWVALID ),
+.control_S_AXI_AWREADY									(wire_control_S_AXI_AWREADY ),
+.control_S_AXI_WDATA									(wire_control_S_AXI_WDATA ),
+.control_S_AXI_WSTRB									(wire_control_S_AXI_WSTRB  ),
+.control_S_AXI_WVALID									(wire_control_S_AXI_WVALID  ),
+.control_S_AXI_WREADY									(wire_control_S_AXI_WREADY  ),
+.control_S_AXI_BRESP									(wire_control_S_AXI_BRESP  ),
+.control_S_AXI_BVALID									(wire_control_S_AXI_BVALID  ),
+.control_S_AXI_BREADY									(wire_control_S_AXI_BREADY  ),
+.control_S_AXI_ARADDR									(wire_control_S_AXI_ARADDR [8:0] ), // MISMATCH [11 : 9]
+.control_S_AXI_ARVALID									(wire_control_S_AXI_ARVALID ),
+.control_S_AXI_ARREADY									(wire_control_S_AXI_ARREADY ),
+.control_S_AXI_RDATA									(wire_control_S_AXI_RDATA  ),
+.control_S_AXI_RRESP									(wire_control_S_AXI_RRESP  ),
+.control_S_AXI_RVALID									(wire_control_S_AXI_RVALID  ),
+.control_S_AXI_RREADY									(wire_control_S_AXI_RREADY  ),
 
 // ENABLE SIGNAL
 .enable_processing										(1'b1), // CONSTANT
 
 // AXIS PACKET OUTPUT INTERFACE
-.packet_out_packet_out_TVALID							(reg_packet_out_packet_out_TVALID  ),
-.packet_out_packet_out_TREADY							(reg_packet_out_packet_out_TREADY  ),
-.packet_out_packet_out_TDATA							(reg_packet_out_packet_out_TDATA   ),
-.packet_out_packet_out_TKEEP							(reg_packet_out_packet_out_TKEEP   ),
-.packet_out_packet_out_TLAST							(reg_packet_out_packet_out_TLAST   ),
+.packet_out_packet_out_TVALID							(wire_packet_out_packet_out_TVALID  ),
+.packet_out_packet_out_TREADY							(wire_packet_out_packet_out_TREADY  ),
+.packet_out_packet_out_TDATA							(wire_packet_out_packet_out_TDATA   ),
+.packet_out_packet_out_TKEEP							(wire_packet_out_packet_out_TKEEP   ),
+.packet_out_packet_out_TLAST							(wire_packet_out_packet_out_TLAST   ),
 
 // TUPLE OUTPUT INTERFACE
 .tuple_out_tuple_out_VALID								(/*VOID*/), // N.C.
@@ -224,15 +242,15 @@ p4_processor p4_processor_inst (
 
 // LINE CLK & RST SIGNALS
 .clk_line_rst											(reg_clk_line_rst ), // INV
-.clk_line												(reg_clk_line ),
+.clk_line												(wire_clk_line ),
 
 // PACKET CLK & RST SIGNALS
 .clk_packet_rst											(reg_clk_packet_rst), // INV
-.clk_packet												(reg_clk_packet),
+.clk_packet												(wire_clk_packet),
 
 // CONTROL CLK & RST SIGNALS
 .clk_control_rst										(reg_clk_control_rst), // INV
-.clk_control											(reg_clk_control),
+.clk_control											(wire_clk_control),
 
 // RST DONE SIGNAL
 .internal_rst_done										(/*VOID*/) // N.C.
@@ -244,6 +262,7 @@ p4_processor p4_processor_inst (
 //#################################
 
 always @ ( posedge axis_aclk /*or posedge S_AXI_ACLK*/ )
+
 begin
 
 // TUSER
