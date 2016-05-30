53290a64256a74d59e4fa313a73da4191a071196
Tu Dang <huynh.tu.dang@usi.ch>
2016-05-30 17:35:47 +0200

    clean up

--- debug/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BMV2_PATH=~/bmv2
-P4C_BM_PATH=~/p4c-bmv2
-P4C_BM_SCRIPT=$P4C_BM_PATH/p4c_bm/__main__.py
-SWITCH_PATH=$BMV2_PATH/targets/simple_switch/simple_switch
-CLI_PATH=$BMV2_PATH/targets/simple_switch/sswitch_CLI
