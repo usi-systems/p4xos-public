8b12c23a1abebe589e0993c9325c4004bd9eda24
Tu Dang <huynh.tu.dang@usi.ch>
2016-08-31 22:09:51 +0200

    script to setup DPDK after restart

---
+++ dpdk/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ] ;  then
+	echo "Please run as root"
+	echo "sudo -E ./setup.sh"
+	exit -1
+fi
+
+echo 1024 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+echo 1024 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+
+modprobe uio_pci_generic
+
+$RTE_SDK/tools/dpdk_nic_bind.py --status
+$RTE_SDK/tools/dpdk_nic_bind.py --bind=uio_pci_generic 01:00.0
+$RTE_SDK/tools/dpdk_nic_bind.py --bind=uio_pci_generic 01:00.1
+$RTE_SDK/tools/dpdk_nic_bind.py --status
