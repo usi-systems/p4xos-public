f63b4dbba746b8b3603cedb34612930df93288ad
Tu Dang <huynh.tu.dang@usi.ch>
2016-09-01 23:46:23 +0200

    offload IP checksum

--- dpdk/main.c
+++ dpdk/main.c
@@ -239,11 +239,21 @@ calc_latency(uint8_t port __rte_unused, uint16_t qidx __rte_unused,
 static inline int
 port_init(uint8_t port, struct rte_mempool *mbuf_pool, struct learner* learner)
 {
+	struct rte_eth_dev_info dev_info;
+	struct rte_eth_txconf *txconf;
+	struct rte_eth_rxconf *rxconf;
 	struct rte_eth_conf port_conf = port_conf_default;
 	const uint16_t rx_rings = 1, tx_rings = 1;
 	int retval;
 	uint16_t q;
 
+	rte_eth_dev_info_get(port, &dev_info);
+
+	rxconf = &dev_info.default_rxconf;
+	txconf = &dev_info.default_txconf;
+
+	txconf->txq_flags &= PKT_TX_IPV4;
+	txconf->txq_flags &= PKT_TX_UDP_CKSUM;
 	if (port >= rte_eth_dev_count())
 		return -1;
 
@@ -253,14 +263,14 @@ port_init(uint8_t port, struct rte_mempool *mbuf_pool, struct learner* learner)
 
 	for (q = 0; q < rx_rings; q++) {
 		retval = rte_eth_rx_queue_setup(port, q, RX_RING_SIZE,
-				rte_eth_dev_socket_id(port), NULL, mbuf_pool);
+				rte_eth_dev_socket_id(port), rxconf, mbuf_pool);
 		if (retval < 0)
 			return retval;
 	}
 
 	for (q = 0; q < tx_rings; q++) {
 		retval = rte_eth_tx_queue_setup(port, q, TX_RING_SIZE,
-				rte_eth_dev_socket_id(port), NULL);
+				rte_eth_dev_socket_id(port), txconf);
 		if (retval < 0)
 			return retval;
 	}
@@ -355,10 +365,21 @@ check_deliver(struct rte_timer *tim,
 		rte_timer_stop(tim);
 }
 
-static void
+__attribute__((unused))
+static uint16_t
+get_psd_sum(void *l3_hdr, uint16_t ethertype, uint64_t ol_flags)
+{
+	if (ethertype == ETHER_TYPE_IPv4)
+		return rte_ipv4_phdr_cksum(l3_hdr, ol_flags);
+	else /* assume ethertype == ETHER_TYPE_IPv6 */
+		return rte_ipv6_phdr_cksum(l3_hdr, ol_flags);
+}
+
+static uint64_t
 craft_new_packet(struct rte_mbuf **created_pkt, uint32_t srcIP, uint32_t dstIP,
 		uint16_t sport, uint16_t dport, size_t data_size, uint8_t output_port)
 {
+	uint64_t ol_flags = 0;
 	size_t pkt_size = sizeof(struct ether_hdr) + sizeof(struct ipv4_hdr) +
 		sizeof(struct udp_hdr) + data_size;
 	(*created_pkt)->data_len = pkt_size;
@@ -377,6 +398,8 @@ craft_new_packet(struct rte_mbuf **created_pkt, uint32_t srcIP, uint32_t dstIP,
 	iph->dst_addr = rte_cpu_to_be_32(dstIP);
 	iph->version_ihl = 0x45;
 	iph->hdr_checksum = 0;
+	ol_flags |= PKT_TX_IPV4;
+	ol_flags |= PKT_TX_IP_CKSUM;
 	iph->total_length = rte_cpu_to_be_16( sizeof(struct ipv4_hdr) +
 			sizeof(struct udp_hdr) + sizeof(paxos_message));
 	iph->next_proto_id = IPPROTO_UDP;
@@ -384,21 +407,29 @@ craft_new_packet(struct rte_mbuf **created_pkt, uint32_t srcIP, uint32_t dstIP,
 	udp = (struct udp_hdr *)((unsigned char*)iph + sizeof(struct ipv4_hdr));
 	udp->src_port = rte_cpu_to_be_16(sport);
 	udp->dst_port = rte_cpu_to_be_16(dport);
+	//ol_flags |= PKT_TX_UDP_CKSUM;
 	udp->dgram_cksum = 0;
 	udp->dgram_len = rte_cpu_to_be_16(data_size);
+	return ol_flags;
 }
 
 static void
 send_repeat_message(paxos_message *pm) {
 	uint8_t port_id = 1;
 	struct rte_mbuf *created_pkt = rte_pktmbuf_alloc(mbuf_pool);
-	craft_new_packet(&created_pkt, IPv4(192,168,4,95), IPv4(239,3,29,73),
+	created_pkt->l2_len = sizeof(struct ether_hdr);
+	created_pkt->l3_len = sizeof(struct ipv4_hdr);
+	created_pkt->l4_len = sizeof(struct udp_hdr) + sizeof(paxos_message);
+	uint64_t ol_flags = craft_new_packet(&created_pkt, IPv4(192,168,4,95), IPv4(239,3,29,73),
 			34951, 34952, sizeof(paxos_message), port_id);
-	size_t paxos_offset = sizeof(struct ether_hdr) + sizeof(struct ipv4_hdr) +
-				sizeof(struct udp_hdr);
-	paxos_message *px = rte_pktmbuf_mtod_offset(created_pkt, struct paxos_message *,
-				paxos_offset);
+	//struct udp_hdr *udp;
+	size_t udp_offset = sizeof(struct ether_hdr) + sizeof(struct ipv4_hdr);
+	//udp  = rte_pktmbuf_mtod_offset(created_pkt, struct udp_hdr *, udp_offset);
+	size_t paxos_offset = udp_offset + sizeof(struct udp_hdr);
+	paxos_message *px = rte_pktmbuf_mtod_offset(created_pkt, paxos_message *, paxos_offset);
 	rte_memcpy(px, pm, sizeof(*pm));	
+	//udp->dgram_cksum = get_psd_sum(udp, ETHER_TYPE_IPv4, ol_flags);
+	created_pkt->ol_flags = ol_flags;
 	const uint16_t nb_tx = rte_eth_tx_burst(port_id, 0, &created_pkt, 1);
 	rte_pktmbuf_free(created_pkt);
 	rte_log(RTE_LOG_DEBUG, RTE_LOGTYPE_USER8, "Send %d repeated messages\n", nb_tx);
