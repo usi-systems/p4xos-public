ffaf272b5f95b19f5b6dd7f53307b70551a1eae7
jabolina <jose.bolina@neppoadm.com.br>
2019-01-29 10:02:22 -0200

    fix make and demo

--- bmv2/run_demo.sh
+++ bmv2/run_demo.sh
@@ -18,18 +18,22 @@ THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 source $THIS_DIR/env.sh
 
-PROG="paxos"
+P4_SRC=../p4src
+ACCEPTOR="$P4_SRC/acceptor"
+COORDINATOR="$P4_SRC/coordinator"
+LEARNER="$P4_SRC/learner"
 
 P4C_BM_SCRIPT=$P4C_BM_PATH/p4c_bm/__main__.py
-
 SWITCH_PATH=$BMV2_PATH/targets/simple_switch/simple_switch
-
 CLI_PATH=$BMV2_PATH/targets/simple_switch/sswitch_CLI
 
-$P4C_BM_SCRIPT ../p4src/$PROG.p4 --json $PROG.json
+$P4C_BM_SCRIPT $ACCEPTOR.p4 --json $ACCEPTOR.json
+$P4C_BM_SCRIPT $COORDINATOR.p4 --json $COORDINATOR.json
+$P4C_BM_SCRIPT $LEARNER.p4 --json $LEARNER.json
+
 sudo PYTHONPATH=$PYTHONPATH:$BMV2_PATH/mininet/ python topo.py \
     --behavioral-exe $BMV2_PATH/targets/simple_switch/simple_switch \
-    --acceptor $PROG.json \
-    --coordinator $PROG.json \
+    --acceptor $ACCEPTOR.json \
+    --coordinator $LEARNER.json \
     --cli $CLI_PATH \
     --start-server
